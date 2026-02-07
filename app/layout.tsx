4fc477deff9d96002285afa2252abe8ffbcd400d
Malo Miquel <malomiquel@gmail.com>
2026-02-07 20:02:46 +0100

    fix: adblocker tracker

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
         {process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID && (
           <Script
             defer
-            src="https://cloud.umami.is/script.js"
+            src="/stats/script.js"
             data-website-id={process.env.NEXT_PUBLIC_UMAMI_WEBSITE_ID}
           />
         )}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => [
+    {
+      source: "/stats/script.js",
+      destination: "https://cloud.umami.is/script.js",
+    },
+    {
+      source: "/stats/:path*",
+      destination: "https://cloud.umami.is/:path*",
+    },
+  ],
 };
 
 export default nextConfig;
